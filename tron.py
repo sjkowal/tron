ffd068817c472a090307b81d0e9b4e91ac88e74f
Steven Kowal <sjkowal@gmail.com>
2013-09-28 09:11:08 -0500

    Updated readme markdown with license

--- tron.py
+++ tron.py
@@ -20,8 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
 import procgame.game
+
+#minor change
 
 class FirstMode(procgame.game.Mode):
   def __init__(self, game):
