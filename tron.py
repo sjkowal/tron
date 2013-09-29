f453acd3625c3bb611bb813503b5f616ab0fe901
Steven Kowal <sjkowal@gmail.com>
2013-09-29 15:23:35 -0500

    Setup basic game logic and add files handing the ball trough and attract mode

--- tron.py
+++ tron.py
@@ -21,33 +21,106 @@
 # THE SOFTWARE.
 
 import procgame.game
-
-#minor change
+import trough
 
 class FirstMode(procgame.game.Mode):
-  def __init__(self, game):
-    super(FirstMode, self).__init__(game=game, priority=5)
+    def __init__(self, game):
+        super(FirstMode, self).__init__(game=game, priority=5)
 
-  def sw_startButton_active(self, sw):
-    print("Start!")
-    self.game.lamps.startButton.schedule(schedule=0xff00ff00,cycle_seconds=0, now=True)
-    self.game.coils.popB.pulse(50)
-    self.game.lamps.shootAgain.pulse(50) # Turn on indefinitely.
-    return procgame.game.SwitchStop
+    def sw_startButton_active(self, sw):
+        self.game.lamps.startButton.schedule(schedule=0xff00ff00,cycle_seconds=0, now=True)
+        self.game.coils.popB.pulse(50)
+        self.game.lamps.shootAgain.pulse(50) # Turn on indefinitely.
+        return procgame.game.SwitchStop
 
 
-class ExampleGame(procgame.game.GameController):
-  def __init__(self, machine_type):
-    super(ExampleGame, self).__init__(machine_type)
-    self.load_config('tron.yaml')
+class BaseGameMode(procgame.game.Mode):
+    def __init(self, game):
+        super(BaseGameMode, self).__init__(game=game, priority=1)
+        pass
 
-  def reset(self):
-    super(ExampleGame, self).reset()
-    first_mode = FirstMode(self)
-    self.modes.add(first_mode)
-    self.enable_flippers(enable=True)
+    def mode_started(self):
+        self.game.trough.changed_handlers.append(self.trough_changed)
 
-game = ExampleGame(machine_type='sternSAM')
-game.reset()
-game.run_loop()
+    def mode_stopped(self):
+        self.game.trough.changed_handlers.remove(self.trough_changed)
+
+    def trough_changed(self):
+        if self.game.trough.is_full()
+            self.game.end_ball()
+
+    def sw_popL_active_for_200ms(self, sw):
+        self.game.coils.popL.pulse()
+        self.game.lamps.popL.pulse(50)
+
+    def sw_popR_active_for_200ms(self, sw):
+        self.game.coils.popR.pulse()
+        self.game.lamps.popR.pulse(50)
+
+    def sw_popB_active_for_200ms(self, sw):
+        self.game.coils.popB.pulse()
+        self.game.lamps.popB.pulse(50)
+
+
+class TronGame(procgame.game.BasicGame):
+
+    trough = None
+    basic_game_mode = None
+
+    def __init__(self, machine_type):
+        super(ExampleGame, self).__init__('sternSAM')
+        self.load_config('tron.yaml')
+        self.trough = trough.Trough(game=self)
+        self.base_game_mode = BaseGameMode(game=self)
+
+    def reset(self):
+        super(ExampleGame, self).reset()
+        self.modes.add(self.trough)
+        self.modes.add(self.attract_mode)
+
+    def start_ball(self)
+        super(TronGame, self).start_ball()
+
+    def game_started(self)
+        self.log("GAME STARTED")
+        super(TronGame, self).game_started()
+        # don't start_ball here
+
+    def ball_starting(self)
+        self.log("BALL STARTING")
+        super(TronGame, self).ball_starting()
+        # TODO: Frist Check for ball in shooter lane
+        # TODO: Next, puslse trough until ball shows up in the
+        # shooter lane
+       
+        # Eject a ball into the shooter lane.
+        self.coils.trough.pulse()
+
+        self.enable_flippers(True)
+        self.modes.add(self.base_game_mode)
+
+    def ball_ended(self):
+        """Called by end_ball(), which is itslef called by base_game_mode.trough_changed """
+        self.log("BALL ENDED")
+        self.modes.remove(self.base_game_mode)
+        self.enable_flippers(False)
+        super(TronGame, self).ball_ended()
+
+    def game_ended(self):
+        self.log("GAME_ENDED")
+        super(TronGame, self).game_ended()
+        self.modes.remove(self.base_game_mode)
+        self.modds.add(self.attract_mode)
+
+## main:
+def main():
+    game = None
+    try:
+        game = TronGame()
+        game.runloop()
+    finally:
+        del game
+
+if __name__ == '__main__'
+    main()
 
